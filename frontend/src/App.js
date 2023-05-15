da8f7d44aa15d27bcdc420e5d5a9bd7a2f2aa580
zhua633 <zhua633@aucklanduni.ac.nz>
2023-05-15 22:43:49 +1200

    set up tailwind and file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Projects from "./Components/Projects/index";
+import Footer from "./Components/Footer/index";
+import Title from "./Components/Title";
+import Heading from "./Components/Heading";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title/>
+      <Heading text="Projects"/>
+      <Projects/>
+      <Footer/>
     </div>
-  );
-}
-
+  )
+  }
 export default App;
---
+++ frontend/src/Components/Heading/index.jsx
@@ -0,0 +1,8 @@
+function Title({text}) {
+    return (
+      <div class="w-full justify-center flex p-5">
+          <span class="mb-4 font-bold text-gray-900 dark:text-white md:text-4xl lg:text-5xl">{text}</span>
+      </div>
+    )
+    }
+  export default Title;
