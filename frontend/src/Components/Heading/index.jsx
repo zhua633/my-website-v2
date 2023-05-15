93d5f85df82a51bca0561add12c4e3ea1ed7305b
zhua633 <zhua633@aucklanduni.ac.nz>
2023-05-15 22:51:46 +1200

    Edit title underline colour

--- frontend/src/Components/Heading/index.jsx
+++ frontend/src/Components/Heading/index.jsx
@@ -1,7 +1,7 @@
 function Title({text}) {
     return (
       <div class="w-full justify-center flex p-5">
-          <span class="mb-4 font-bold text-gray-900 dark:text-white md:text-4xl lg:text-5xl">{text}</span>
+          <span class="mb-4 font-bold text-gray-900 dark:text-white lg:text-4xl underline underline-offset-3 decoration-8 decoration-blue-600 dark:decoration-blue-600">{text}</span>
       </div>
     )
     }
--- frontend/src/Components/Title/index.jsx
+++ frontend/src/Components/Title/index.jsx
@@ -3,6 +3,7 @@ function Title() {
     <div class="flex items-center justify-center h-screen">
     <div class="text-center">
         <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Hi, I'm <mark class="px-2 text-white bg-blue-600 rounded dark:bg-blue-500"> Anvilly</mark> Huang </h1>
+
         <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">A graduate SWE. I like to code.</p>
     </div>
     </div>
