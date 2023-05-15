550011bd71e1952d3f07ae408b07c613445596d9
zhua633 <zhua633@aucklanduni.ac.nz>
2023-05-16 00:21:18 +1200

    Update projects

--- frontend/src/Components/Projects/index.jsx
+++ frontend/src/Components/Projects/index.jsx
@@ -1,14 +1,14 @@
-import MyComponent from "../Helpers/card/index";
+import Card from "../Helpers/card/index";
 function Projects() {
   return (
     <div className="w-full justyify-around flex flex-wrap gap-y-10 justify-around">
     <div className="w-full flex p-10 space-x-5 justify-around">
-      <MyComponent title="My Title" description="Hi there"/>
-      <MyComponent title="My Title" description="hahahahah"/>
+      <Card title="To-do List" description="Gamify daily tasks to boost productivity using Golang." link="https://github.com/zhua633/go-to-do-list"/>
+      <Card title="OpenAI Atari Games" description="Used TensorFlow to train OpenAI classic control and Atari games with machine learning models. Optimised the scoring and used AI to teach computer to play games!" link="https://github.com/zhua633/COMPSYS726-Reinforcement-Learning" />
     </div>
     <div className="w-full flex p-10 space-x-5 justify-around">
-      <MyComponent title="My Title" description="Hi there"/>
-      <MyComponent title="My Title" description="hahahahah"/>
+      <Card title="Old Personal Website" description="Old legacy personal website developed using basic CSS and React concepts." link="https://zhua633.github.io/my-website/"/>
+      <Card title="Tic Tac Toe" description="Used alpha beta pruning to teach the computer to play a game of Tic Tac Toe. Be careful because the computer almost always win!" link="https://github.com/zhua633/Tic-Tac-Toe" />
     </div>
     </div>
   )
