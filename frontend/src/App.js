feb575c3690c94aa2b57d24ddddce7063ad19ee3
zhua633 <zhua633@aucklanduni.ac.nz>
2023-05-15 23:40:50 +1200

    Add timeline

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import Projects from "./Components/Projects/index";
 import Footer from "./Components/Footer/index";
 import Title from "./Components/Title";
 import Heading from "./Components/Heading";
+import Timeline from "./Components/Timeline";
+
 function App() {
   return (
     <div>
       <Title/>
+      <Heading text="Experiences"/>
+      <Timeline/>
       <Heading text="Projects"/>
       <Projects/>
       <Footer/>
--- frontend/src/Components/Projects/index.jsx
+++ frontend/src/Components/Projects/index.jsx
@@ -1,7 +1,7 @@
 import MyComponent from "../Helpers/card/index";
 function Projects() {
   return (
-    <div className="w-full justyify-around flex flex-wrap gapp-y-10 justify-around">
+    <div className="w-full justyify-around flex flex-wrap gap-y-10 justify-around">
     <div className="w-full flex p-10 space-x-5 justify-around">
       <MyComponent title="My Title" description="Hi there"/>
       <MyComponent title="My Title" description="hahahahah"/>
